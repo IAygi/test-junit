278d551a32ec55258da0078ac2b4093576e0d84e
Innokentiy Aygi <iaygi@fil-it.ru>
2024-05-18 09:21:00 +0300

    first commit

---
+++ src/test/java/ru/grv/SimpleTest.java
@@ -0,0 +1,13 @@
+package ru.grv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    void firstTest() {
+        assertTrue(false);
+    }
+}
